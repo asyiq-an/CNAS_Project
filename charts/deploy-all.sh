ee8b777bda48ae5fb47b07b33c52bdca49091a6a
Asyiq Nuruddin <94519945+AsyiqNuruddin@users.noreply.github.com>
2025-07-24 16:31:37 +0800

    Update deploy-all.sh
    asf

--- charts/deploy-all.sh
+++ charts/deploy-all.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+# bruh
 kubectl apply -f namespace.yaml
-kubectl apply -f mysql-secret.yaml -n cnas-crud-php
-kubectl apply -f mysql-init.yaml -n cnas-crud-php
-kubectl apply -f mysql-deployment.yaml -n cnas-crud-php
-kubectl apply -f mysql-service.yaml -n cnas-crud-php
-kubectl apply -f create-service-deployment.yaml -n cnas-crud-php
-kubectl apply -f create-service-service.yaml -n cnas-crud-php
-kubectl apply -f update-service-deployment.yaml -n cnas-crud-php
-kubectl apply -f update-service-service.yaml -n cnas-crud-php
-kubectl apply -f delete-service-deployment.yaml -n cnas-crud-php
-kubectl apply -f delete-service-service.yaml -n cnas-crud-php
-kubectl apply -f query-service-deployment.yaml -n cnas-crud-php
-kubectl apply -f query-service-service.yaml -n cnas-crud-php
-kubectl apply -f ingress.yaml -n cnas-crud-php
+kubectl apply -f templates/mysql-secret.yaml -n cnas-crud-php
+kubectl apply -f templates/mysql-init.yaml -n cnas-crud-php
+kubectl apply -f templates/mysql-deployment.yaml -n cnas-crud-php
+kubectl apply -f templates/mysql-service.yaml -n cnas-crud-php
+kubectl apply -f templates/create-service-deployment.yaml -n cnas-crud-php
+kubectl apply -f templates/create-service-service.yaml -n cnas-crud-php
+kubectl apply -f templates/update-service-deployment.yaml -n cnas-crud-php
+kubectl apply -f templates/update-service-service.yaml -n cnas-crud-php
+kubectl apply -f templates/delete-service-deployment.yaml -n cnas-crud-php
+kubectl apply -f templates/delete-service-service.yaml -n cnas-crud-php
+kubectl apply -f templates/query-service-deployment.yaml -n cnas-crud-php
+kubectl apply -f templates/query-service-service.yaml -n cnas-crud-php
+kubectl apply -f templates/ingress.yaml -n cnas-crud-php
+
