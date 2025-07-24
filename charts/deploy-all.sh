627838f1601697fc9e4b5e1dd2b9b29acb38db72
Asyiq Nuruddin <94519945+AsyiqNuruddin@users.noreply.github.com>
2025-07-24 16:49:15 +0800

    Update deploy-all.sh

--- charts/deploy-all.sh
+++ charts/deploy-all.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-# bruh
-kubectl apply -f namespace.yaml
-kubectl apply -f templates/mysql-secret.yaml -n cnas-crud-php
-kubectl apply -f templates/mysql-init.yaml -n cnas-crud-php
-kubectl apply -f templates/mysql-deployment.yaml -n cnas-crud-php
-kubectl apply -f templates/mysql-service.yaml -n cnas-crud-php
-kubectl apply -f templates/create-service-deployment.yaml -n cnas-crud-php
-kubectl apply -f templates/create-service-service.yaml -n cnas-crud-php
-kubectl apply -f templates/update-service-deployment.yaml -n cnas-crud-php
-kubectl apply -f templates/update-service-service.yaml -n cnas-crud-php
-kubectl apply -f templates/delete-service-deployment.yaml -n cnas-crud-php
-kubectl apply -f templates/delete-service-service.yaml -n cnas-crud-php
-kubectl apply -f templates/query-service-deployment.yaml -n cnas-crud-php
-kubectl apply -f templates/query-service-service.yaml -n cnas-crud-php
-kubectl apply -f templates/ingress.yaml -n cnas-crud-php
 
+# Apply namespace from root
+kubectl apply -f namespace.yaml
+
+# Apply all templates
+kubectl apply -f charts/templates/mysql-secret.yaml -n cnas-crud-php
+kubectl apply -f charts/templates/mysql-init.yaml -n cnas-crud-php
+kubectl apply -f charts/templates/mysql-deployment.yaml -n cnas-crud-php
+kubectl apply -f charts/templates/mysql-service.yaml -n cnas-crud-php
+kubectl apply -f charts/templates/create-service-deployment.yaml -n cnas-crud-php
+kubectl apply -f charts/templates/create-service-service.yaml -n cnas-crud-php
+kubectl apply -f charts/templates/update-service-deployment.yaml -n cnas-crud-php
+kubectl apply -f charts/templates/update-service-service.yaml -n cnas-crud-php
+kubectl apply -f charts/templates/delete-service-deployment.yaml -n cnas-crud-php
+kubectl apply -f charts/templates/delete-service-service.yaml -n cnas-crud-php
+kubectl apply -f charts/templates/query-service-deployment.yaml -n cnas-crud-php
+kubectl apply -f charts/templates/query-service-service.yaml -n cnas-crud-php
+kubectl apply -f charts/templates/ingress.yaml -n cnas-crud-php
