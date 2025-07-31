27a3ebd5c049b661f227ad142cb8f3ca76ae85a4
AsyiqNuruddin <nuruddinasyiq@gmail.com>
2025-07-31 10:04:33 +0800

    Depl
    oy

--- charts/deploy-all.sh
+++ charts/deploy-all.sh
@@ -6,33 +6,15 @@ NAMESPACE="cnas-crud-php"
 echo "[INFO] Deleting CNAS Deployments and Services..."
 
 # Delete Deployments
-kubectl delete deployment create-service -n $NAMESPACE || true
-kubectl delete deployment update-service -n $NAMESPACE || true
-kubectl delete deployment delete-service -n $NAMESPACE || true
-kubectl delete deployment query-service -n $NAMESPACE || true
-kubectl delete deployment mysql -n $NAMESPACE || true
-
-# Delete Services
-kubectl delete service create-service -n $NAMESPACE || true
-kubectl delete service update-service -n $NAMESPACE || true
-kubectl delete service delete-service -n $NAMESPACE || true
-kubectl delete service query-service -n $NAMESPACE || true
-kubectl delete service mysql -n $NAMESPACE || true
-
-# Delete Ingress (if applicable)
-kubectl delete ingress cnas-ingress -n $NAMESPACE || true
-
-# Optionally delete PVCs, secrets, configmaps if they’re dynamic
-# kubectl delete pvc --all -n $NAMESPACE || true
-# kubectl delete secret mysql-secret -n $NAMESPACE || true
+kubectl delete namespace $NAMESPACE
 
 echo "[INFO] Applying updated manifests..."
 
 kubectl apply -f namespace.yaml
+
 kubectl apply -f charts/templates/mysql-secret.yaml -n $NAMESPACE
 kubectl apply -f charts/templates/mysql-init.yaml -n $NAMESPACE
 kubectl apply -f charts/templates/mysql-deployment.yaml -n $NAMESPACE
-kubectl apply -f charts/templates/mysql-pvc.yaml -n $NAMESPACE
 kubectl apply -f charts/templates/mysql-service.yaml -n $NAMESPACE
 
 kubectl apply -f charts/templates/create-service-deployment.yaml -n $NAMESPACE
